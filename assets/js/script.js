fdf11f82bc7cc2c0fb2920b0e4c37939653479c2
Aaron Liu <aaron155658@gmail.com>
2022-07-03 16:43:12 -0400

    improve task-list section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
 // declare formEl and save as memory to save-task
-var formEl = document.querySelector("#task-form");
+var formEl = document.querySelector("#save-task");
 // declare tasksToDoEl and save as memory to tasks-to-do
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
 
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    //give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput +"</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.append(listItemEl);
 };
 
